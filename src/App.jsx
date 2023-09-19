0005e3ef580ed5e9dd4526e3bdc3961351a50c3a
sandeep <mspreddy2021@gmail.com>
2023-09-19 07:18:51 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+function App() {
+  return (
+    <>
+      <Button variant="contained">Contained</Button>
+      <h1 className="">App</h1>
+    </>
+  );
+}
+
+export default App;
